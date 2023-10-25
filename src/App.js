e9b0f7bb596d4045104171d104ed0613883649c1
kak_reak <timberr.4t@gmail.com>
2023-10-25 23:39:55 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 
 function App() {
-    return (
-        <div className="App">
-            <Router>
-                <Routes>
-                    <Route path={"/"} element={<Login/>}/>
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<Login />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -7,8 +7,10 @@ const Login = (props) => {
                 <CTA>
                     <CTALogoOne src={"/images/cta-logo-one.svg"}/>
                     <SignUp>GET ALL THERE</SignUp>
-                    <Description>Get Premier Access ti Raya and the Last Dragon for an additional fee with a Disney+
-                        subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                    <Description>
+                        Get Premier Access ti Raga and the Last Dragon for an additional fee
+                        with a Disney+ subscription. As of 03/26/21, the price of Disney+
+                        and The Disney Bundle will increase by $1.
                     </Description>
                     <CTALogoTwo src={"/images/cta-logo-two.png"}/>
                 </CTA>
@@ -16,7 +18,7 @@ const Login = (props) => {
             </Content>
         </Container>
     );
-}
+};
 
 const Container = styled.section`
   overflow: hidden;
